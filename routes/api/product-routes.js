5d237fa767a7c6e1cd9f0fa9a062ea21a3c17e79
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-29 13:37:57 +1000

    updated gitignore

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,12 +36,10 @@ router.get('/:id', async (req, res) => {
         }
       ]
     });
-
     if (!productData) {
       res.status(404).json({ message: 'No product was found with that id!' });
       return;
     }
-
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -144,8 +142,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+      id: req.params.id,
+    }
+    });
+    if (!deleteProduct) {
+      res.status(404).json({ message: 'No product was found with that id!' });
+      return;
+    }
+    res.status(200).json(deleteProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
