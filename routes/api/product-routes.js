24a2ad05b04e2ff0fc1891ccb4d116abecad26c7
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-29 12:23:27 +1000

    post to products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -52,28 +52,20 @@ router.get('/:id', async (req, res) => {
 
 
 // create new product
-router.post('/', (req, res) => {
-
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+router.post('/', async (req, res) => {
+  try {
+    const newProduct = await Product.create({
+      id: req.body.id,
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id
     });
+
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // update product
