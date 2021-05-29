0341c67d8ac1bf9b56db3fa1350f24d67df6a675
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-29 13:26:51 +1000

    categories PUT request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,11 +53,37 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(
+      {category_name: req.body.category_name},
+      {where: {
+      id: req.params.id}
+    });
+    if (!updateCategory) {
+      res.status(404).json({ message: 'No category was found with that id!' });
+      return;
+    }
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+      id: req.params.id,
+    }
+    });
+    if (!deleteCategory) {
+      res.status(404).json({ message: 'No category was found with that id!' });
+      return;
+    }
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
