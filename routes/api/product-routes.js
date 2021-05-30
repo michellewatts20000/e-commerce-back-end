d280b729e3bd0e5faf5a8f14eb0ad6a44b2001dc
Michelle Watts <watts.e.michelle@gmail.com>
2021-05-30 16:12:46 +1000

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -49,34 +49,10 @@ router.get('/:id', async (req, res) => {
   
 
 
-// create new product
-// router.post('/', async (req, res) => {
-//   try {
-//     const newProduct = await Product.create({
-//       id: req.body.id,
-//       product_name: req.body.product_name,
-//       price: req.body.price,
-//       stock: req.body.stock,
-//       category_id: req.body.category_id
-//     });
-
-//     res.status(200).json(newProduct);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
 
 // create new product
 
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       console.log('req.body', req.body);
